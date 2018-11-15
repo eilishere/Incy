513251686704f1211174ef1da670177094ab97aa
Rishad Wahid <rbwahid@gmail.com>
2018-11-15 11:41:45 +0600

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=incy-Web-Deploy.pubxml -p:Password=pvZlLmCsm3cor4RocKfir0gdfqXGSNbZs0G7bkGbJTNs4PTdz3uXeuKxynGq
+
